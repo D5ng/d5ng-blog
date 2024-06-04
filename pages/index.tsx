ffe6af66a5bc1b24836fe3f04f1d2d3e9db275fa
d5ng <dong5ffice@gmail.com>
2024-06-04 18:57:44 +0900

    Feat: 블로그 페이지 추가 및 파일 이동

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,3 @@
-import { GetStaticProps, InferGetStaticPropsType } from "next"
-import { getCategoryList } from "@/lib/post.lib"
-
-export default function HomePage(props: InferGetStaticPropsType<typeof getStaticProps>) {
-  return (
-    <ul>
-      {props.categoryList.map((category) => (
-        <li key={category}>{category}</li>
-      ))}
-    </ul>
-  )
+export default function HomePage() {
+  return <div>HomePage</div>
 }
-
-export const getStaticProps = (async () => {
-  const categoryList = getCategoryList()
-  return {
-    props: {
-      categoryList,
-    },
-  }
-}) satisfies GetStaticProps
