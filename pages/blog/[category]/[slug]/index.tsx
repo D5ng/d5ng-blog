958ac666c868c19c9c44b54b603a30db2fa7c056
d5ng <dong5ffice@gmail.com>
2024-06-05 02:54:49 +0900

    [#8] Feat: 상세 페이지 추가 및 SSG 설정

--- pages/blog/[category]/[slug]/index.tsx
+++ pages/blog/[category]/[slug]/index.tsx
@@ -1,9 +1,19 @@
-import { getPostDetail, getPostPaths, parsePostAbstract } from "@/lib/post.lib"
-import { GetStaticPaths, GetStaticProps } from "next"
 import React from "react"
+import { GetStaticPaths, GetStaticProps, InferGetStaticPropsType } from "next"
+import { getPostDetail, getPostPaths, parsePostAbstract } from "@/lib/post.lib"
+import { serialize } from "next-mdx-remote/serialize"
+import { MDXRemote } from "next-mdx-remote"
 
-export default function PostDetailPage() {
-  return <div>index</div>
+export default function PostDetailPage(props: InferGetStaticPropsType<typeof getStaticProps>) {
+  return (
+    <div>
+      <div>{props.title}</div>
+      <div>{props.description}</div>
+      <div>읽는 시간: {props.readingMinutes}분</div>
+      <div>{props.categoryPath}</div>
+      <MDXRemote {...props.mdxSource} />
+    </div>
+  )
 }
 
 export const getStaticPaths = (async () => {
@@ -21,11 +31,12 @@ export const getStaticProps = (async (context) => {
   const category = context.params!.category as string
   const slug = context.params!.slug as string
   const post = getPostDetail(category, slug)
-  console.log(post)
+  const mdxSource = await serialize(post.content)
 
   return {
     props: {
-      post,
+      ...post,
+      mdxSource,
     },
   }
 }) satisfies GetStaticProps
