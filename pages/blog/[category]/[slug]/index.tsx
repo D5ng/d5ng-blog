f2b689999c3dac8b3cb08ec1d8f52c7865c95763
d5ng <dong5ffice@gmail.com>
2024-06-05 02:47:48 +0900

    [#8] Feat: 포스트 상세 페이지 추가

--- pages/blog/[category]/[slug]/index.tsx
+++ pages/blog/[category]/[slug]/index.tsx
@@ -1,5 +1,31 @@
+import { getPostDetail, getPostPaths, parsePostAbstract } from "@/lib/post.lib"
+import { GetStaticPaths, GetStaticProps } from "next"
 import React from "react"
 
 export default function PostDetailPage() {
   return <div>index</div>
 }
+
+export const getStaticPaths = (async () => {
+  const paths = getPostPaths()
+    .map((path) => parsePostAbstract(path))
+    .map((data) => ({ params: { category: data.categoryPath, slug: data.slug } }))
+
+  return {
+    paths,
+    fallback: false,
+  }
+}) satisfies GetStaticPaths
+
+export const getStaticProps = (async (context) => {
+  const category = context.params!.category as string
+  const slug = context.params!.slug as string
+  const post = getPostDetail(category, slug)
+  console.log(post)
+
+  return {
+    props: {
+      post,
+    },
+  }
+}) satisfies GetStaticProps
