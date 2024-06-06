2823b2f70776dc5050a16c2ffa0b91e89f773e1a
d5ng <dong5ffice@gmail.com>
2024-06-07 02:09:00 +0900

    Refactor: 불 필요한 코드 지우기

--- components/darkmode/darkmode.tsx
+++ components/darkmode/darkmode.tsx
@@ -6,8 +6,7 @@ import { Button } from "@/components/ui/button"
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "@/components/ui/dropdown-menu"
 
 export function ModeToggle() {
-  const { theme, setTheme } = useTheme()
-  console.log(theme)
+  const { setTheme } = useTheme()
 
   return (
     <DropdownMenu>
