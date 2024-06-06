5c83040d0252a1c0c51ed1e06c8770af7a2a8ad7
d5ng <dong5ffice@gmail.com>
2024-06-07 02:18:28 +0900

    [#13] Feat: ui/button style 변경 및 다크모드 수정

--- components/darkmode/darkmode.tsx
+++ components/darkmode/darkmode.tsx
@@ -10,8 +10,8 @@ export function ModeToggle() {
 
   return (
     <DropdownMenu>
-      <DropdownMenuTrigger asChild className="border-none">
-        <Button variant="outline" size="icon">
+      <DropdownMenuTrigger asChild>
+        <Button variant="outline" size="icon" className="px-4 py-1 rounded-full w-fit border">
           <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
           <MoonIcon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
           <span className="sr-only">Toggle theme</span>
--- components/header/header.tsx
+++ components/header/header.tsx
@@ -4,16 +4,16 @@ import { ModeToggle } from "../darkmode/darkmode"
 
 export default function Header() {
   return (
-    <header className="px-5 py-4 flex justify-between items-center">
+    <header className="px-5 py-5 flex justify-between items-center">
       <h1 className="text-2xl font-bold">
         <Link href="/">D5ng</Link>
       </h1>
 
       <ul className="flex gap-x-2">
-        <li className="px-4 py-1 rounded-full border border-neutral-900 w-fit">
+        <li>
           <ModeToggle />
         </li>
-        <li className="px-4 py-1 rounded-full border border-neutral-900 w-fit flex justify-center items-center">
+        <li className="px-4 py-1 rounded-full border w-fit flex justify-center items-center">
           <Link href="/">Github</Link>
         </li>
       </ul>
