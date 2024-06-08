0866a2a9579a7b9e450c0ef12017766c5586c245
d5ng <dong5ffice@gmail.com>
2024-06-09 02:42:17 +0900

    Feat: 포스트 상세페이지 프로필 컴포넌트 추가 v2

--- pages/blog/[category]/[slug]/index.tsx
+++ pages/blog/[category]/[slug]/index.tsx
@@ -2,16 +2,27 @@ import { GetStaticPaths, GetStaticProps, InferGetStaticPropsType } from "next"
 import { getPostDetail, getPostPaths, parsePostAbstract } from "@/lib/post.lib"
 import { serialize } from "next-mdx-remote/serialize"
 import { MDXRemote } from "next-mdx-remote"
+// @ts-expect-error no types
+import remarkA11yEmoji from "@fec/remark-a11y-emoji"
+import remarkGfm from "remark-gfm"
+import remarkBreaks from "remark-breaks"
+
+import rehypeSlug from "rehype-slug"
+import rehypePrettyCode from "rehype-pretty-code"
+import rehypeAutolinkHeadings from "rehype-autolink-headings"
+// import rehypePrism from "rehype-prism-plus"
+import { Badge } from "@/components/ui/badge"
+import PostDetailProfile from "@/components/post/post-detail-profile"
 
 export default function PostDetailPage(props: InferGetStaticPropsType<typeof getStaticProps>) {
   return (
-    <div className="prose bg-background text-foreground">
-      <div>{props.title}</div>
-      <div>{props.description}</div>
-      <div>읽는 시간: {props.readingMinutes}분</div>
-      <div>{props.categoryPath}</div>
+    <section className="m-auto prose prose-neutral dark:prose-invert max-w-[680px]">
+      <Badge className="mb-4 px-2.5 py-1">{props.categoryPath.toUpperCase()}</Badge>
+      <h1>{props.title}</h1>
+      <p className="text-neutral-500">{props.description}</p>
+      <PostDetailProfile author={props.author} readingMinutes={props.readingMinutes} date={props.date} />
       <MDXRemote {...props.mdxSource} />
-    </div>
+    </section>
   )
 }
 
@@ -26,16 +37,6 @@ export const getStaticPaths = (async () => {
   }
 }) satisfies GetStaticPaths
 
-import remarkGfm from "remark-gfm"
-import remarkBreaks from "remark-breaks"
-// @ts-expect-error no types
-import remarkA11yEmoji from "@fec/remark-a11y-emoji"
-
-import rehypeSlug from "rehype-slug"
-import rehypePrism from "rehype-prism-plus"
-import rehypePrettyCode from "rehype-pretty-code"
-import rehypeAutolinkHeadings from "rehype-autolink-headings"
-
 export const getStaticProps = (async (context) => {
   const category = context.params!.category as string
   const slug = context.params!.slug as string
@@ -45,7 +46,18 @@ export const getStaticProps = (async (context) => {
     scope: {},
     mdxOptions: {
       remarkPlugins: [remarkGfm, remarkA11yEmoji, remarkBreaks],
-      rehypePlugins: [[rehypePrettyCode, { theme: { dark: "github-dark-dimmed", light: "github-light" } }]],
+      rehypePlugins: [
+        rehypeSlug,
+        [rehypePrettyCode, { theme: { dark: "github-dark-dimmed", light: "github-light" } }],
+        [
+          rehypeAutolinkHeadings,
+          {
+            properties: {
+              className: ["anchor"],
+            },
+          },
+        ],
+      ],
     },
   })
 
