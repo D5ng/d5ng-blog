0e0180d517c9514eb5a8008648439fc87f311a71
d5ng <dong5ffice@gmail.com>
2024-06-08 21:22:58 +0900

    [#15] Feat: Footer UI 추가

---
+++ components/footer/footer.tsx
@@ -0,0 +1,28 @@
+export default function Footer() {
+  return (
+    <footer className="mt-20">
+      <div className="border-t flex justify-between">
+        <div className="relative flex flex-col border-r px-10 py-12 flex-2 w-3/12 [&_span+span]:mt-1">
+          <span className="font-medium [&+span]:mt-2">Info</span>
+          <span className="text-sm">Github</span>
+          <span className="text-sm">Porfoilo</span>
+        </div>
+        <div className="relative flex flex-col border-r px-10 py-12 w-4/12"></div>
+        <div className="flex flex-col px-10 py-12 w-5/12 [&_span+span]:mt-1">
+          <span className="font-medium [&+span]:mt-2">Skill</span>
+          <span className="text-sm">HTML</span>
+          <span className="text-sm">CSS</span>
+          <span className="text-sm">JavaScript</span>
+          <span className="text-sm">TypeScript</span>
+          <span className="text-sm">React</span>
+          <span className="text-sm">Next</span>
+        </div>
+      </div>
+      <div className="border-t flex justify-between items-center px-10 py-12">
+        <span className="text-sm">© 2024</span>
+        <span className="text-sm">DongHyun all rights reserved.</span>
+        <span className="text-sm">FrontEnd</span>
+      </div>
+    </footer>
+  )
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,7 @@ import type { AppProps } from "next/app"
 import { Poppins } from "next/font/google"
 
 import "@/styles/globals.css"
+import Footer from "@/components/footer/footer"
 
 const poppins = Poppins({
   weight: ["400", "500", "700"],
@@ -23,6 +24,7 @@ export default function App({ Component, pageProps }: AppProps) {
       </>
       <Header />
       <Component {...pageProps} />
+      <Footer />
     </ThemeProvider>
   )
 }
