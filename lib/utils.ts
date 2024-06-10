eeebd41e0851d0845a4ea14595c911a7764e6ccf
d5ng <dong5ffice@gmail.com>
2024-06-11 01:19:11 +0900

    Refactor: [#21] 포스트 디테일 리액션 컴포넌트 테마 분리하기

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,23 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function getThemeMode(theme: "dark" | "light" | "system") {
+  return theme === "dark" || theme === "system"
+    ? {
+        colors: {
+          text: "#FAFAFA",
+          background: "#0A0A0A",
+          primary: "#FAFAFA",
+          icon: "#FAFAFA",
+        },
+      }
+    : {
+        colors: {
+          text: "#0A0A0A",
+          background: "#fff",
+          primary: "#171717",
+          icon: "#171717",
+        },
+      }
+}
