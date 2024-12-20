42e67e772d6fcd37f02cb23b2071403935dbb8f7
d5ng <dong5ffice@gmail.com>
2024-12-20 09:50:03 +0900

    ✨ Feat: Canvas 웹 접근성 대응

--- components/three/scene.tsx
+++ components/three/scene.tsx
@@ -1,10 +1,14 @@
 import { Canvas } from "@react-three/fiber"
 import Model from "./model"
+import Image from "next/image"
 
 export default function Scene(props: { image: string }) {
   return (
-    <Canvas>
-      <Model image={props.image} />
-    </Canvas>
+    <>
+      <Image src={props.image} alt="post thumnbnail" fill className="hidden" />
+      <Canvas>
+        <Model image={props.image} />
+      </Canvas>
+    </>
   )
 }
